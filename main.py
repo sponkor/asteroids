c71043b0b489ee313010429edd56271fc1e576a3
sponkor <caleb.kostroun@outlook.com>
2025-01-13 05:42:19 -0600

    2: Added FPS regulation

--- main.py
+++ main.py
@@ -5,10 +5,21 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True is True: #infinite while loop, used to draw visuals
+    clock = pygame.time.Clock()
+    dt = 0
+    
+
+
+    while True is True: #infinite while loop, used to update game
         pygame.Surface.fill(screen,000)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        dt = clock.tick(60) / 1000
 
-
+        
+        
+        
         pygame.display.flip()
 
 
